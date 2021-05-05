d2ad104d2efb2b6b8fdc347312bd80095930da1a
Jesse Rogers <jesserileyrogers@gmail.com>
2021-05-05 17:48:31 -0400

    header partial and base styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register' }, controllers: { registrations: 'registrations' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, controllers: { registrations: 'registrations' }
 end
