28bbbbe35e1bb26ddfe7c5f8b3e41b637da58e82
Jesse Rogers <jesserileyrogers@gmail.com>
2021-05-17 14:57:25 -0400

    registration form styling

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+
+    def sign_up_params
+        params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
 end
---
+++ config/initializers/field_with_errors.rb
@@ -0,0 +1,7 @@
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
+    unless html_tag =~ /^<label/
+      "<div class=\"field_with_errors\">#{html_tag}</div>".html_safe
+    else
+      html_tag.html_safe
+    end
+  end
