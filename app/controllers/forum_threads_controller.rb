448b2619a1c8c5ddafa2322a12c6400bb01e49a6
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-07 14:36:33 -0400

    new forum thread form

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,6 +1,27 @@
 class ForumThreadsController < ApplicationController
 
+    def new
+        @forum_thread = ForumThread.new
+    end
+
+    def create
+        @forum_thread = ForumThread.new(forum_thread_params)
+        @forum_thread.author_id = current_user.id
+
+        if @forum_thread.save
+            redirect_to forum_thread_path(@forum_thread)
+        else
+            render :new
+        end
+    end
+
     def show
         @forum_thread = ForumThread.find(params[:id])
+    end
+
+    private
+
+    def forum_thread_params
+        params.require(:forum_thread).permit(:title, :body, :forum_id)
     end
 end
