ede4ed2432dc2389869919184ff3bd7500a39ada
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-08 22:09:34 -0400

    dependent destroy for forum models

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,6 +1,6 @@
 class Forum < ApplicationRecord
     belongs_to :channel
-    has_many :forum_threads
+    has_many :forum_threads, dependent: :destroy
 
     scope :ordered_by_position, -> { order(:position) }
 end
--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -2,5 +2,5 @@ class ForumThread < ApplicationRecord
     belongs_to :forum
     belongs_to :author, class_name: "User", foreign_key: "author_id"
 
-    has_many :forum_replies
+    has_many :forum_replies, dependent: :destroy
 end
