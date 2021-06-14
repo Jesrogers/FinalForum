f6a4db75bca796e497a3c30702f763df3bc4fa6c
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-14 13:07:20 -0400

    fixed some link styling errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,6 @@ Bill = User.create!(username: "Billinator", email: "test@gmail.com", password: "
 User.create!(username: "Freddy", email: "testy@gmail.com", password: "testtest", password_confirmation: "testtest")
 User.create!(username: "Coolguyjohn", email: "testyy@gmail.com", password: "testtest", password_confirmation: "testtest")
 
-Bill.add_role(:admin)
-
 Channel.create(name: 'Employees', position: 1)
 Channel.create(name: 'Departments', position: 1)
 
@@ -25,7 +23,10 @@ ForumThread.create(forum_id: 1, author_id: 1, title: "This company is great!", b
 ForumThread.create(forum_id: 1, author_id: 3, title: "Wow, does anyone else...", body: "<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. <p>", created_at: "2021-03-05 12:22:48.662059000 +0000", updated_at: "2021-03-05 12:22:48.662059000 +0000")
 ForumThread.create(forum_id: 1, author_id: 2, title: "Who has exciting plans for the weekend?", body: "<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>", created_at: "2019-08-05 12:22:48.662059000 +0000", updated_at: "2019-08-05 12:22:48.662059000 +0000")
 ForumThread.create(forum_id: 1, author_id: 1, title: "The weather is getting crazy hot", body: "<p>Suspendisse sollicitudin imperdiet velit, in eleifend sapien</p>", created_at: "2020-07-05 12:22:48.662059000 +0000", updated_at: "2020-07-05 12:22:48.662059000 +0000")
+ForumThread.create(forum_id: 2, author_id: 3, title: "I'm selling all the best stuff!", body: "<p>Who wants to buy it?</p>", created_at: "2020-03-05 06:41:42.662059000 +0000", updated_at: "2021-06-09 19:13:21.662059000 +0000")
 
 ForumReply.create(forum_thread_id: 1, author_id: 2, body: "<p>Collaboratively administrate empowered markets via plug-and-play networks.</p> <ol><li>Dynamically procrastinate B2C users</li><li>Dramatically visualize customers</li><li>Efficiently unleash cross-media information</li></ol><p>Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>", created_at: "2021-06-02 13:18:48.662059000 +0000", updated_at: "2021-06-02 13:18:48.662059000 +0000")
 ForumReply.create(forum_thread_id: 1, author_id: 3, body: "<p>What are you guys <strong>talking</strong> about? I'm so confused...</p>", created_at: "2021-06-04 14:03:48.662059000 +0000", updated_at: "2021-06-04 14:03:48.662059000 +0000")
 ForumReply.create(forum_thread_id: 1, author_id: 1, body: "<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures. Completely iterate covalent strategic theme areas via accurate e-markets.</p><p>Globally incubate standards compliant channels before scalable benefits. Quickly disseminate superior deliverables whereas web-enabled applications. Quickly drive clicks-and-mortar catalysts for change before vertical architectures.</p>", created_at: "2021-06-08 12:49:48.662059000 +0000", updated_at: "2021-06-08 12:49:48.662059000 +0000")
+
+Bill.add_role(:admin)
