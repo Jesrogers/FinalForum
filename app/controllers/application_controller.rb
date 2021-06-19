f1ef00bac389bce9e26b87088404edf7ec302860
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-19 17:30:44 -0400

    added 403 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
-    render "errors/not_found", status: 404
+    render "errors/forbidden", status: 403
   end
 end
