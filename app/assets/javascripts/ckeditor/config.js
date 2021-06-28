4b028c03d17ae231d05d7714328b0ccd2307bd4a
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-28 10:44:08 -0400

    moved ckeditor files back to og location, modified options and plugins

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -10,6 +10,7 @@ CKEDITOR.editorConfig = function (config) {
 
     /* Filebrowser routes */
     // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+
     config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
 
     // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
@@ -19,38 +20,31 @@ CKEDITOR.editorConfig = function (config) {
     config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
 
     // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
-    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+    config.filebrowserImageBrowseLinkUrl = "";
 
     // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
-    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+    config.filebrowserImageBrowseUrl = "";
 
     // The location of a script that handles file uploads in the Image dialog.
-    config.filebrowserImageUploadUrl = "/ckeditor/pictures?";
+    config.filebrowserImageUploadUrl = "";
 
     // The location of a script that handles file uploads.
     config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 
     config.allowedContent = true;
     config.filebrowserUploadMethod = 'form';
+    config.removeDialogTabs = 'image:Upload;image:advanced';
+    config.extraPlugins = 'justify';
 
     // Toolbar groups configuration.
     config.toolbar = [
         { name: 'document', groups: ['mode', 'document', 'doctools'], items: ['Source'] },
-        { name: 'clipboard', groups: ['clipboard', 'undo'], items: ['Cut', 'Copy', 'Paste', 'PasteText', '-', 'Undo', 'Redo'] },
-        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
-        { name: 'insert', items: ['HorizontalRule', 'SpecialChar'] },
-        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'], items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
-        '/',
-        { name: 'styles', items: ['Format', 'Font', 'FontSize'] },
-        { name: 'colors', items: ['TextColor', 'BGColor'] },
-        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'], items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] }
-    ];
-
-    config.toolbar_mini = [
-        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'], items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
-        { name: 'styles', items: ['Font', 'FontSize'] },
-        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'clipboard', groups: ['clipboard', 'undo'], items: ['Undo', 'Redo'] },
         { name: 'basicstyles', groups: ['basicstyles', 'cleanup'], items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] },
-        { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar'] }
+        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'], items: ['Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'NumberedList', 'BulletedList'] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'insert', items: ['Image', 'HorizontalRule', 'SpecialChar'] },
+        { name: 'styles', items: ['Format', 'Font', 'FontSize'] },
+
     ];
 };
