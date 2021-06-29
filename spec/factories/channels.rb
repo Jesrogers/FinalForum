852b64760ee91307f7429f4a98c8189d9593a755
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-28 23:03:32 -0400

    start channel model tests

--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :channel do
-    
+    name { "Employees" }
+    position { 1 }
   end
 end
--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Channel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:channel) { FactoryBot.create(:channel) }
+
+  it "is valid with a name" do
+    expect(channel).to be_valid
+  end
+
+  it "is not valid without a name" do
+    channel = Channel.new(name: nil)
+    expect(channel).to_not be_valid
+  end
 end
