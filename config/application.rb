b51e5c3bab071e3aa280e00c2d67e552e5535e04
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-28 20:35:03 -0400

    setup rspec and spring, removed test folder

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,15 @@ module FinalForum
     config.action_view.sanitized_allowed_tags = ['p', 'strong', 'em', 'a', 'img', 'blockquote', 'hr', 'ul', 'ol', 'li', 'br']
     config.action_view.sanitized_allowed_attributes = ['href', 'title', 'style', 'src', 'target', 'id', 'name'] 
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false,
+        controller_specs: false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- test/controllers/user_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class UserControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get user_show_url
-    assert_response :success
-  end
-end
