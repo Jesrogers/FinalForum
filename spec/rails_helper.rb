cebe48f175b7b9cc2054c9a5c856e2ffe156af96
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-30 10:18:47 -0400

    moved simplecov start declaration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/'
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
