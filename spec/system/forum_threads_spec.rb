5db1009a5b3d02e0ebfae528b860cb0243dbb232
Jesse Rogers <jesserileyrogers@gmail.com>
2021-07-04 15:14:57 -0400

    started thread tests for admins

--- spec/system/forum_threads_spec.rb
+++ spec/system/forum_threads_spec.rb
@@ -115,6 +115,20 @@ RSpec.describe "ForumThreads", type: :system do
       expect(page).to have_text("403 Forbidden")
     end
 
+    it "doesn't allow for thread creation when forum is locked" do
+      thread = FactoryBot.create(:forum_thread, forum: locked_forum, author: user, title: "This is a test thread", body: "Hello, please ignore this thread. It is being used for testing. Thanks!")
+      
+      sign_in user
+      visit root_path
+      click_link "Forums"
+      click_link(href: "/forums/#{locked_forum.slug}")
+      expect(page).to have_text("This forum is locked. Feel free to browse, but interaction is disabled.")
+      expect(page).to_not have_link("New Thread", href: new_forum_forum_thread_path(locked_forum))
+
+      visit new_forum_forum_thread_path(locked_forum)
+      expect(page).to have_text("403 Forbidden")
+    end
+
     it "doesn't allow for editing of own threads when forum is locked" do
       thread = FactoryBot.create(:forum_thread, forum: locked_forum, author: user, title: "This is a test thread", body: "Hello, please ignore this thread. It is being used for testing. Thanks!")
       
@@ -171,10 +185,90 @@ RSpec.describe "ForumThreads", type: :system do
       expect(page).to_not have_link("Delete", href: forum_thread_path(locked_thread))
     end
 
+    it "renders new page with validation errors" do
+      sign_in user
+      visit root_path
+      click_link "Forums"
+      click_link(href: "/forums/#{forum.slug}")
+      expect(page).to have_link("New Thread", href: new_forum_forum_thread_path(forum.id))
 
+      click_link("New Thread", href: new_forum_forum_thread_path(forum.id))
+
+      fill_in "Title", with: "I am a brand new thread! I am a brand new thread! I am a brand new thread! I am a brand new thread! Hello!"
+      fill_in_ckeditor('forum_thread_body', with: "Hello")
+      click_button "Submit"
+
+      expect(page).to have_text("Title is too long (maximum is 100 characters)")
+      expect(page).to have_text("Body is too short (minimum is 20 characters)")
+    end
+
+    it "renders edit page with validation errors" do
+      thread = FactoryBot.create(:forum_thread, author: user, title: "This is a test thread", body: "Hello, please ignore this thread. It is being used for testing. Thanks!")
+      
+      sign_in user
+      visit root_path
+      click_link "Forums"
+      click_link(href: "/forums/#{thread.forum.slug}")
+      click_link(href: forum_thread_path(thread))
+      expect(page).to have_link("Edit", href: edit_forum_thread_path(thread))
+
+      click_link("Edit", href: edit_forum_thread_path(thread))
+      fill_in("Title", with: "I am a brand new thread! I am a brand new thread! I am a brand new thread! I am a brand new thread! Hello!")
+      fill_in_ckeditor("forum_thread_body", with: "Ignore")
+      click_button("Submit")
+
+      expect(page).to have_text("Title is too long (maximum is 100 characters)")
+      expect(page).to have_text("Body is too short (minimum is 20 characters)")
+    end
   end
 
   context "as a logged in admin" do
+    let(:admin_user) { FactoryBot.create(:user, :admin) }
+    let(:forum) { FactoryBot.create(:forum) }
+    let(:locked_forum) { FactoryBot.create(:forum, :locked) }
+
+    it "allows for threads to be created when forum is unlocked" do
+      sign_in admin_user
+      visit root_path
+      click_link "Forums"
+      click_link(href: forum_path(forum), class: "forum-card__link")
+
+      expect(page).to have_link("New Thread", href: new_forum_forum_thread_path(forum.id))
+
+      visit new_forum_forum_thread_path(forum)
+      expect(page).to have_field("Title")
+      expect(page).to have_css("#cke_forum_thread_body")
+
+      fill_in "Title", with: "I am a brand new thread!"
+      fill_in_ckeditor('forum_thread_body', with: "I like long walks on the beach")
+      click_button "Submit"
+
+      expect(page).to have_current_path("/threads/i-am-a-brand-new-thread")
+      expect(page).to have_text("I am a brand new thread!")
+      expect(page).to have_text("I like long walks on the beach")
+    end
+
+    it "allows for threads to be created when forum is locked" do
+      sign_in admin_user
+      visit root_path
+      click_link "Forums"
+      click_link(href: forum_path(locked_forum), class: "forum-card__link")
+
+      expect(page).to have_text("This forum is locked. Feel free to browse, but interaction is disabled.")
+      expect(page).to have_link("New Thread", href: new_forum_forum_thread_path(locked_forum.id))
+      
+      visit new_forum_forum_thread_path(forum)
+      expect(page).to have_field("Title")
+      expect(page).to have_css("#cke_forum_thread_body")
+
+      fill_in "Title", with: "I am a brand new thread!"
+      fill_in_ckeditor('forum_thread_body', with: "I like long walks on the beach")
+      click_button "Submit"
+
+      expect(page).to have_current_path("/threads/i-am-a-brand-new-thread")
+      expect(page).to have_text("I am a brand new thread!")
+      expect(page).to have_text("I like long walks on the beach")
+    end
   end
 
   def fill_in_ckeditor(locator, options)
